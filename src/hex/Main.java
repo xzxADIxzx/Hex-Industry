32a5ecd6762eac56175caf58e1599fbb944a5590
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-01-01 16:32:26 +0300

    cleanup

---
+++ src/hex/Main.java
@@ -0,0 +1,16 @@
+package hex;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class Main extends Plugin {
+
+	@Override
+	public void init() {}
+
+	@Override
+	public void registerServerCommands(CommandHandler handler) {}
+
+	@Override
+	public void registerClientCommands(CommandHandler handler) {}
+}

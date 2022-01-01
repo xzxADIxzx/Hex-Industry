37452ce44219c6354b18b35542e8000f12872df3
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-01-01 15:50:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### Setup
+
+Clone this repository first.
+To edit the plugin display name and other data, take a look at `plugin.json`.
+Edit the name of the project itself by going into `settings.gradle`.
+
+### Basic Usage
+
+See `src/example/ExamplePlugin.java` for some basic commands and event handlers.  
+Every main plugin class must extend `Plugin`. Make sure that `plugin.json` points to the correct main plugin class.
+
+Please note that the plugin system is in beta, and as such is subject to changes.
+
+### Building a Jar
+
+`gradlew jar` / `./gradlew jar`
+
+Output jar should be in `build/libs`.
+
+
+### Installing
+
+Simply place the output jar from the step above in your server's `config/mods` directory and restart the server.
+List your currently installed plugins/mods by running the `mods` command.
---
+++ src/example/ExamplePlugin.java
@@ -0,0 +1,84 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.Administration.*;
+import mindustry.world.blocks.storage.*;
+
+public class ExamplePlugin extends Plugin{
+
+    //called when game initializes
+    @Override
+    public void init(){
+        //listen for a block selection event
+        Events.on(BuildSelectEvent.class, event -> {
+            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
+                //player is the unit controller
+                Player player = event.builder.getPlayer();
+
+                //send a message to everyone saying that this player has begun building a reactor
+                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
+            }
+        });
+
+        //add a chat filter that changes the contents of all messages
+        //in this case, all instances of "heck" are censored
+        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
+
+        //add an action filter for preventing players from doing certain things
+        Vars.netServer.admins.addActionFilter(action -> {
+            //random example: prevent blast compound depositing
+            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
+                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
+                return false;
+            }
+            return true;
+        });
+    }
+
+    //register commands that run on the server
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+        handler.register("reactors", "List all thorium reactors in the map.", args -> {
+            for(int x = 0; x < Vars.world.width(); x++){
+                for(int y = 0; y < Vars.world.height(); y++){
+                    //loop through and log all found reactors
+                    //make sure to only log reactor centers
+                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor && Vars.world.tile(x, y).isCenter()){
+                        Log.info("Reactor at @, @", x, y);
+                    }
+                }
+            }
+        });
+    }
+
+    //register commands that player can invoke in-game
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+        //register a simple reply command
+        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
+            player.sendMessage("You said: [accent] " + args[0]);
+        });
+
+        //register a whisper command which can be used to send other players messages
+        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
+            //find player by name
+            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
+
+            //give error message with scarlet-colored text if player isn't found
+            if(other == null){
+                player.sendMessage("[scarlet]No player by that name found!");
+                return;
+            }
+
+            //send the other player a message, using [lightgray] for gray text color and [] to reset color
+            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+        });
+    }
+}

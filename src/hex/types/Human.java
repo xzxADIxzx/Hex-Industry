24ed9dbb84f9ba4b22e7f05f8e35f2da82bae3d5
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-01-02 20:35:55 +0300

    fix

--- src/hex/types/Human.java
+++ src/hex/types/Human.java
@@ -3,6 +3,7 @@ package hex.types;
 import hex.*;
 import mindustry.gen.*;
 import mindustry.game.*;
+import mindustry.world.*;
 import mindustry.content.*;
 
 import static mindustry.Vars.*;
@@ -15,17 +16,21 @@ public class Human {
 	public Hex citadel;
 	public Fraction fraction;
 
-	public Human(Player ppl, Fraction abilities){
+	public Human(Player ppl, Fraction abilities) {
 		player = ppl;
 		fraction = abilities;
 	}
 
-	public void init(Hex hex){
+	public void init(Hex hex) {
 		this.citadel = hex;
 		player.team(Team.baseTeams[id++]);
 
 		// TODO: move to hex.build
-		world.tile(hex.x, hex.y).setNet(Blocks.coreNucleus, player.team(), 0);
-		Schems.citadel.tiles.each(st -> world.tile(st.x + hex.x + 2, st.y + hex.y + 3).setBlock(st.block));
+		world.tile(hex.cx, hex.cy).setNet(Blocks.coreNucleus, player.team(), 0);
+		Schems.citadel.tiles.each(st -> {
+			Tile tile = world.tile(st.x + hex.x + 2, st.y + hex.y + 3);
+			tile.setNet(st.block, player.team(), 0);
+			if (st.config != null) tile.build.configureAny(st.config);
+		});
 	}
 }

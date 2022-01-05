363ae934f706a224fa0eb360ec815d463c2d1b1e
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-01-05 23:58:58 +0300

    ...

--- src/hex/types/BuilderAI.java
+++ src/hex/types/BuilderAI.java
@@ -6,9 +6,13 @@ public class BuilderAI extends AIController {
 
 	public boolean start;
 
+	public BuilderAI() {
+		timer.get(200f); // reset
+	}
+
 	@Override
 	public void updateMovement() {
-		if (!start) unit.updateBuilding = start = timer.get(3, 500);
+		if (!start) unit.updateBuilding = start = timer.get(3, 200f);
 
 		if (unit.buildPlan() != null) moveTo(unit.buildPlan().tile(), 200f);
 		else unit.spawnedByCore(true);

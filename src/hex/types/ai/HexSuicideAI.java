4453549212294abcb0fa0f8ae9af2ef3eeee474f
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-02-09 13:31:24 +0300

    crutches from hopelessness

--- src/hex/types/ai/HexSuicideAI.java
+++ src/hex/types/ai/HexSuicideAI.java
@@ -1,54 +1,58 @@
 package hex.types.ai;
 
 import arc.graphics.Color;
+import arc.math.Mathf;
 import arc.math.geom.Vec2;
+import arc.struct.Seq;
 import hex.types.Hex;
 import mindustry.ai.Pathfinder;
-import mindustry.ai.Pathfinder.PositionTarget;
 import mindustry.content.Fx;
 import mindustry.content.Items;
 import mindustry.entities.units.AIController;
 import mindustry.gen.Call;
-import mindustry.world.Tile;
 import mindustry.world.meta.BlockFlag;
 
 import static arc.Core.app;
 import static hex.Main.hexes;
+import static mindustry.Vars.tilesize;
 import static mindustry.Vars.world;
-import static mindustry.Vars.pathfinder;
 
 public class HexSuicideAI extends AIController {
 
     public int state;
 
     public Hex hex;
-    public PositionTarget cultivator;
+    public Seq<Vec2> marks;
 
     public HexSuicideAI() {
         app.post(() -> {
             hex = hexes.min(h -> h.pos().dst(unit));
-            cultivator = new PositionTarget(new Vec2(hex.fx + 16f, hex.fy + 8f));
-            state = 1;
+            marks = Seq.with(from(7, -3), from(3, -3), from(3, 1));
         });
     }
 
     @Override
     public void updateMovement() {
-        if (state == 1) {
-            Tile tile = pathfinder.getTargetTile(unit.tileOn(), cultivator);
-            unit.movePref(vec.trns(unit.angleTo(tile.worldx(), tile.worldy()), unit.speed()));
+        if (state < marks.size) {
+            Vec2 pos = marks.get(state);
+            unit.movePref(vec.trns(unit.angleTo(pos.getX(), pos.getY()), unit.speed()));
 
-            if (unit.within(tile, 10f)) state = 2;
-        } else if (state == 2) {
-            Call.takeItems(world.build(hex.cx, hex.cy + 1), Items.sporePod, 30, unit);
-            if (unit.hasItem()) state = 3;
+            if (unit.within(pos, 2f)) state++;
         } else if (state == 3) {
+            Call.takeItems(world.build(hex.cx, hex.cy + 1), Items.sporePod, 30, unit);
+            state = 4;
+        } else if (state == 4) {
             pathfind(Pathfinder.fieldRally);
+            target = targetFlag(unit.x, unit.y, BlockFlag.rally, false);
 
-            if (unit.within(targetFlag(unit.x, unit.y, BlockFlag.rally, false), 10f)) {
-                unit.controlWeapons(true);
+            if (unit.within(target, 10f)) {
                 Call.effect(Fx.spawn, unit.x, unit.y, 0, Color.white);
+                unit.controlWeapons(true);
             }
         }
+    }
+
+    public Vec2 from(int x, int y) {
+        return new Vec2(hex.fx + x * tilesize + Mathf.random(8f), hex.fy + y * tilesize + Mathf.random(8f));
     }
 }
